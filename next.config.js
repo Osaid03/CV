e5cec97db1f060d4f8dcc7dbc7ed1cd509ef85ea
Osaid <osaid03>
2025-04-04 16:27:58 +0400

    minor fixes on logic

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/CV',
+    trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
